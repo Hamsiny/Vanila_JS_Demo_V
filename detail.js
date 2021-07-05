2649d0bd6573b452438b344a925130c031a185cd
曲洪澎 <hamsiny@quhongpengdeMacBook-Pro.local>
2021-07-05 13:56:20 +1200

    add comments

--- detail.js
+++ detail.js
@@ -1,21 +1,25 @@
+// function to get param detail form param name
 function getUrlParams(paramName) {
     const searchUrl = window.location.search.substring(1);
     const urlParams = new URLSearchParams(searchUrl);
     return urlParams.get(paramName);
 }
 
+// get product by id from rawdata
 function getProduct(id) {
     return rawdata.filter(product => product.prodId == id);
 }
 
+//get prod id, title and product to show
 let prodId = parseInt(getUrlParams('prodId'));
 let prodTitle = getUrlParams('prodTitle');
 let currentProduct = getProduct(prodId);
 let prodDescription = currentProduct[0].description;
 let imgUrl = "https://storage.googleapis.com/luxe_media/wwwroot/" + currentProduct[0].productMedia[0].url;
 
-console.log(currentProduct);
+// console.log(currentProduct);
 
+// show product details to html
 document.getElementById('prodId').innerHTML = 'Product ID: ' + prodId;
 document.getElementById('prodTitle').innerHTML = prodTitle;
 document.getElementById('prodDescription').innerHTML = prodDescription;
